9a2ac4b96fa2637d502f482c79f30a424e4bb4c7
Antonio Rando <antonio.rando@topdigital.es>
2017-10-27 11:49:38 +0200

    sadasdasdasda

---
+++ AtmelStudioTestX/AtmelStudioTestX/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 27/10/2017 10:50:20
+ * Author : Usuario
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ AtmelStudioTestX/AtmelStudioTestX/sketch_oct27a/Sketch.cpp
@@ -0,0 +1,18 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+void setup() {
+  pinMode(4,OUTPUT);
+
+}
+
+void loop() {
+	digitalWrite(4,LOW);
+
+}
